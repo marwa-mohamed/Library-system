5c4ce6cef63839d2dd0b3103a1d9d81708980912
marwa <marwamohamed250@gmail.com>
2022-10-01 12:04:54 +0200

    print menu

---
+++ system.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int choice {};
+
+//printing menu;
+void menu()
+{
+    cout << "Library menu: "<< endl;
+    cout << "1) add book "<< endl;
+    cout << "2) search book by prefix "<< endl;
+    cout << "3) print who borrowed book by name "<< endl;
+    cout << "4) print books by ID "<< endl;
+    cout << "5) print books by name "<< endl;
+    cout << "6) add user "<< endl;
+    cout << "7) user borrow book "<< endl;
+    cout << "8) user print book "<< endl;
+    cout << "9) print users "<< endl;
+    cout << "10) Exit "<< endl;
+    cout << "Enter your choice :";
+    cin >> choice;
+}
+
+
+
+
+
+
+int main()
+{
+    while (true)
+    {
+        menu();
+        switch (choice)
+        {
+            case 1:
+            {
+
+                break;
+            }
+            case 2:
+            {
+
+                break;
+            }
+            case 3:
+            {
+
+                break;
+            }
+            case 4:
+            {
+
+                break;
+            }
+            case 5:
+            {
+
+                break;
+            }
+            case 6:
+            {
+
+                break;
+            }
+            case 7:
+            {
+
+                break;
+            }
+            case 8:
+            {
+
+                break;
+            }
+            case 9:
+            {
+
+                break;
+            }
+            case 10:
+            {
+
+                break;
+            }
+        }
+
+    }
+}
